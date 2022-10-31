24ca032c08e89535f76ca25d0435d33a7be6be17
mikapm <mikapm@met.no>
2022-10-31 16:05:01 -0400

    Copy input angles so they don't get changed.

--- roxsi_pyfuns/coordinate_transforms.py
+++ roxsi_pyfuns/coordinate_transforms.py
@@ -33,23 +33,26 @@ def uvw2enu(vel, heading, pitch, roll, magdec, deg_in=True):
     ux = vel[:,0].copy()
     vy = vel[:,1].copy()
     wz = vel[:,2].copy()
+    hd = heading.copy()
+    pt = pitch.copy()
+    rl = roll.copy()
 
     # Convert angles to radians if needed
     if deg_in:
-        heading *= np.pi/180
-        pitch *= np.pi/180
-        roll *= np.pi/180
+        hd *= np.pi/180
+        pt *= np.pi/180
+        rl *= np.pi/180
         magdec *= np.pi/180
     # Correct heading for magnetic declination
-    heading += magdec
+    hd += magdec
 
     # Define trigonometric terms
-    cos_h = np.cos(heading)
-    sin_h = np.sin(heading)
-    cos_p = np.cos(pitch)
-    sin_p = np.sin(pitch)
-    cos_r = np.cos(roll)
-    sin_r = np.sin(roll)
+    cos_h = np.cos(hd)
+    sin_h = np.sin(hd)
+    cos_p = np.cos(pt)
+    sin_p = np.sin(pt)
+    cos_r = np.cos(rl)
+    sin_r = np.sin(rl)
 
     # Rotate to level coordinate system
     ul = ux*cos_p + vy*sin_p*sin_r + wz*cos_r*sin_p
