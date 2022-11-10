e7596c6c6bbd10a492a1c3341553a5ec88a9788c
mikapm <mikapm@met.no>
2022-11-10 16:47:35 -0500

    Despike AST signals with GP method.

--- roxsi_pyfuns/preprocess/signature_preprocess.py
+++ roxsi_pyfuns/preprocess/signature_preprocess.py
@@ -15,7 +15,40 @@ from scipy.io import loadmat
 import matplotlib.pyplot as plt
 from datetime import datetime as DT
 from cftime import date2num, num2date
+from astropy.stats import mad_std
 from roxsi_pyfuns import despike as rpd
+
+
+def max_runlen(arr, value):
+     """
+     Find runs of consecutive items in an array.
+     Borrowed from alimanfoo at
+     https://gist.github.com/alimanfoo/c5977e87111abe8127453b21204c1065
+     """ 
+     # ensure array 
+     x = np.asanyarray(arr) 
+     if x.ndim != 1: 
+         raise ValueError('only 1D array supported') 
+     n = x.shape[0] 
+  
+     # handle empty array 
+     if n == 0: 
+         return 0 
+     else: 
+         # find run starts 
+         loc_run_start = np.empty(n, dtype=bool) 
+         loc_run_start[0] = True 
+         np.not_equal(x[:-1], x[1:], out=loc_run_start[1:]) 
+         run_starts = np.nonzero(loc_run_start)[0] 
+  
+         # find run values 
+         run_values = x[loc_run_start] 
+         # find run lengths 
+         run_lengths = np.diff(np.append(run_starts, n)) 
+         # Find max run length = to value 
+         max_run = np.max(run_lengths[run_values==value]) 
+
+         return max_run 
 
 class ADCP():
     """
@@ -75,6 +108,39 @@ class ADCP():
         self.fns = sorted(glob.glob(os.path.join(self.datadir,
             '*S{}A001*.mat'.format(self.ser))))
 
+    def read_mat_times(self, mat=None, fn_mat=None):
+        """
+        Function to read only velocity timestamps from given
+        Signature mat structure.
+
+        Parameters:
+            mat - dict; Signature mat structure with timestamps
+            fn_mat - str; path to .mat file to read for mat structure
+                     (not required if mat dict is given)
+
+        Returns:
+            time_mat - array of Matlab timestamps
+            time_arr - array of timestamps in datetime.datetime
+                       format.
+        """
+        # Check that given either mat dict or filename
+        if mat is None and fn_mat is None:
+            raise ValueError('Must input either mat or fn_mat.')
+
+        if fn_mat is not None:
+            # Read mat structure from file
+            mat = loadmat(fn_mat)
+
+        # Read and convert velocity time array
+        time_mat = mat['Data']['Burst_Time'].item().squeeze()
+        # Convert Matlab times to datetime
+        time_arr = pd.Series(pd.to_datetime(time_mat-719529, 
+                             unit='D'))
+        time_arr = time_arr.dt.to_pydatetime()
+
+        return time_mat, time_arr
+
+
     def loaddata_vel(self, fn_mat, ref_date=pd.Timestamp('2022-06-25'),
                      despike_vel=True, despike_ast=True):
         """
@@ -98,12 +164,11 @@ class ADCP():
         mat = loadmat(fn_mat)
 
         # Read and convert general time array
-        time_mat = mat['Data']['Burst_Time'].item().squeeze() # Time array
-        # Convert Matlab times to datetime
-        time_arr = pd.Series(pd.to_datetime(time_mat-719529, unit='D'))
-        time_arr = time_arr.dt.to_pydatetime()
+        time_mat, time_arr = self.read_mat_times(mat=mat)
+
         # Convert time array to numerical format
-        time_units = 'seconds since {:%Y-%m-%d 00:00:00}'.format(ref_date)
+        time_units = 'seconds since {:%Y-%m-%d 00:00:00}'.format(
+            ref_date)
         time_vals = date2num(time_arr, time_units, 
                              calendar='standard', 
                              has_year_zero=True)
@@ -125,14 +190,30 @@ class ADCP():
         si = s.reindex(time_arr, method='nearest').interpolate()
         # Despike AST signal if requested
         if despike_ast:
+            # Make dataframe for raw & despiked AST signals
+            df_ast = si.to_frame(name='raw')
+            # Add despiked column
+            df_ast['des'] = np.ones_like(si.values) * np.nan
+            # Add column for raw signal minus 20-min mean level
+            df_ast['rdm'] = np.ones_like(si.values) * np.nan
             # Count number of full 20-minute (1200-sec) segments
             t0s = si.index[0] # Start timestamp
             t1s = si.index[-1] # End timestamp
             nseg = np.floor((t1s - t0s).total_seconds() / 1200)
             # Iterate over approx. 20-min long segments
             for sn, seg in enumerate(np.array_split(si, nseg)):
-                print('len(seg): ', len(seg))
+                # Get segment start and end times
+                t0ss = seg.index[0]
+                t1ss = seg.index[-1]
+                print('seg: {} - {}'.format(t0ss, t1ss))
+                # Remove mean from raw signal and save to dataframe
+                df_ast['rdm'].loc[t0ss:t1ss] = seg - np.nanmean(seg)
                 # Despike segment using GP method
+                seg_d, mask_d = self.despike_GP(seg, 
+                                                print_kernel=False,
+                                               )
+                # Save despiked segment to correct indices in df_ast
+                df_ast['des'].loc[t0ss:t1ss] = seg_d
 
         # Velocities from beams 1-4
         vb1 = mat['Data']['Burst_VelBeam1'].item().squeeze()
@@ -148,7 +229,8 @@ class ADCP():
                        'vb2': (['time', 'cell'], vb2),
                        'vb3': (['time', 'cell'], vb3),
                        'vb4': (['time', 'cell'], vb4),
-                       'ast': (['time'], si.values),
+                       'astr': (['time'], df_ast['rdm'].values),
+                       'astd': (['time'], df_ast['des'].values),
                       },
             coords={'time': (['time'], time_arr),
                     'cell': (['cell'], cell_arr)}
@@ -158,15 +240,162 @@ class ADCP():
         return ds
 
 
-    def despike_GP(self, arr, **kwargs):
+    def despike_GP(self, arr, r2thresh=0.9, max_dropouts_perc=20, 
+                   longest_dropout_sec=2, **kwargs):
         """
         Despike wave signal following the Gaussian Process-based
         methodology of Malila et al. (2022),
 
         Parameters:
             arr - 1D wave signal to despike
+            r2thresh - scalar; lower threshold for R^2 of GP fit
+            max_dropouts_perc - scalar; max fraction of allowed missing
+                                values in input array (percent)
+            longest_dropout_sec - scalar; longest consecutive missing
+                                  value (seconds)
             **kwargs for rpd.GP_despike()
+        
+        Returns:
+            out - despiked array with spikes and missing values
+                  replaced with GP mean function.
+            mask - boolean spike mask where 0 means bad sample.
         """
+        # Copy input array
+        z_train = arr.copy() 
+
+        # Set GP length scale bounds based on sampling frequency
+        bmin = self.fs # Lower bound (number of samples)
+        bmax = self.fs * 6 # Upper bound
+        
+        # First remove obvious outliers using fixed MAD threshold
+        median = np.nanmedian(z_train) # Median of the signal
+        # robust STD estimate of the subset
+        MAD = mad_std(z_train, ignore_nan=True) 
+        mask_MAD = np.abs(z_train - median) > 10*MAD 
+        # Set to NaN measurements that lie outside the 
+        # +/- 10MAD range from the median (obvious outliers)
+        z_train[mask_MAD] = np.nan
+
+        # GP despiking step (iterative)
+        z_train -= np.nanmean(z_train) 
+        mask_dropouts = np.isnan(z_train) # Mask for dropouts
+        if mask_dropouts.sum() > 0:
+            # Compute maximum run length
+            max_run = max_runlen(mask_dropouts, 1)
+        else:
+            # If no dropouts => set to zero
+            max_run = 0
+        # If too many dropouts => skip despiking (set arr to all NaN)
+        dropouts_percent = (np.sum(mask_dropouts) / 
+                            len(mask_dropouts)) * 100
+        if dropouts_percent >= max_dropouts_perc:
+            print('Too many dropouts in segment: {}%'.format(
+                dropouts_percent))
+            # Return NaN array and all zeros mask
+            z_desp = np.ones_like(arr) * np.nan
+            mask = np.zeros_like(arr)
+            return z_desp, mask 
+        # If longest dropout > longest allowed 
+        # => also skip despiking (set chunk to all NaN)
+        elif max_run > (self.dt * longest_dropout_sec):
+            print('Longest dropout too long segment: {}sec'.format(
+                max_run*self.dt))
+             # Return NaN array and all zeros mask
+            z_desp = np.ones_like(arr) * np.nan
+            mask = np.zeros_like(arr)
+            return z_desp, mask 
+        else:
+            # First iteration: Detect spikes, but don't replace
+            z_desp, ms1, _, _, _, th1 = rpd.GP_despike(
+                ts=z_train, dropout_mask=~mask_dropouts, 
+                despike=True, length_scale_bounds=(bmin, bmax),
+                score_thresh=r2thresh, **kwargs
+                )
+            if ms1.sum() == len(z_train):
+                # No spikes were found => copy old mask
+                r2_avg = r2thresh + 0.1 # Set arbitrary high r2_avg
+                ms_old = mask_dropouts.copy()
+            else:
+                # Do replacement step if spikes were found
+                # Combine dropout and spike masks for replacement step
+                mask_dropouts_spikes = np.logical_or(
+                    mask_dropouts, ~ms1)
+                # Replace using GP mean function
+                z_desp, _, _, _, _, th1 = rpd.GP_despike(
+                    ts=z_train, dropout_mask=~mask_dropouts_spikes, 
+                    despike=False, print_kernel=False, 
+                    length_scale_bounds=(bmin, bmax), 
+                    )
+
+                # Check minimum R^2 score out of all blocks to decide 
+                # whether to continue despiking
+                r2_min = np.min(th1['score'])
+                # Also compute block-avg R2
+                r2_avg = np.mean(th1['score'])
+                ms_old = mask_dropouts_spikes.copy()
+                # Counter for additional despiking iterations
+                cnt_ds = 1 
+                # Max. number of despiking iterations to avoid 
+                # infinite loop
+                max_cnt = 2 
+                # Copy R^2 scores for each chunk
+                scores_in = th1['score'].copy()
+                while r2_min < r2thresh:
+                    # Perform despiking until all GP regression blocks 
+                    # have R^2 >= r2thresh or until maximum number 
+                    # of iterations is passed
+                    cnt_ds += 1 # Increase counter
+                    if cnt_ds > max_cnt:
+                        print(('Max. No. of GP iterations exceeded ' + 
+                              '-> moving on ... '))
+                        # Force break while loop as soon as max 
+                        # iterations are met
+                        break
+                    print(('r2_min = {:.4f} => Despiking iteration' + 
+                          ' #{}').format(r2_min, cnt_ds))
+                    # Despike using GP and sklearn
+                    z_desp, msn, _, _, _, thn = rpd.GP_despike(
+                        ts=z_train, dropout_mask=~ms_old, 
+                        despike=True, print_kernel=False, 
+                        length_scale_bounds=(bmin, bmax),
+                        score_thresh=r2thresh, scores_in=scores_in,
+                        )
+                    if msn.sum() == len(z_train):
+                        print('No new spikes found')
+                        break
+                    else:
+                        # Update mask for replacement (sr) step
+                        ms_old = np.logical_or(ms_old, ~msn)
+                        # Replace
+                        z_desp, _, _, _, _, thn = rpd.GP_despike(
+                            ts=z_train, dropout_mask=~ms_old, 
+                            despike=False, print_kernel=False, 
+                            length_scale_bounds=(bmin, bmax), 
+                            )
+                        # Update r2_min
+                        print('r2_min (updated): {:.4f}'.format(
+                            np.min(thn['score'])))
+                        r2_min = np.min(thn['score'])
+                        # Also update block-avg R2
+                        r2_avg = np.mean(thn['score'])
+                        # Update scores_in list
+                        scores_in = thn['score'].copy()
+                    
+            # If mean R^2 is high enough, return despiked signal
+            # and spike mask
+            if r2_avg >= r2thresh:
+                # block-avg R2 high enough => return despiked signal
+                print('Returning despiked array ... \n')
+                return z_desp, ms_old
+            else:
+                # Block-avg. R2 too low => raise warning but still
+                # return despiked array
+                print('Block-avg too low: {:.4f} ... \n'.format(
+                    r2_avg))
+                # z_desp = np.ones_like(arr) * np.nan
+                # mask = np.zeros_like(arr)
+                return z_desp, ms_old
+
 
 # Main script
 if __name__ == '__main__':
@@ -250,10 +479,47 @@ if __name__ == '__main__':
         adcp = ADCP(datadir=args.dr, ser=ser, )
         # Loop over raw .mat files and save data as netcdf
         ds_list = [] # Empty list for concatenating datasets
-        for i,fn_mat in enumerate(adcp.fns):
-            # Read mat structure
-            dsv = adcp.loaddata_vel(fn_mat)
-            # Append dataset to list for concatenating
-            # ds_list.append(mat)
+        # Read first mat structure and get start and end timestamps
+        times_mat, times = adcp.read_mat_times(fn_mat=adcp.fns[0])
+        date0 = times[0].date() # Date of first timestamp
+        date1 = times[-1].date() # Date of last timestamp
+        print('t0: {}, t1: {}'.format(date0, date1))
+        # Save all datasets for the same date in list for concatenating
+        dsv_daily = []
+        for i,fn_mat in enumerate([adcp.fns[0]]):
+            # Check if daily netcdf files already exist
+            fn_nc0 = os.path.join(outdir, 
+                'Asilomar_SSA_L1_Sig_Vel_{}_{}.nc'.format(
+                    self.mid, date0))
+            fn_nc1 = os.path.join(outdir, 
+                'Asilomar_SSA_L1_Sig_Vel_{}_{}.nc'.format(
+                    self.mid, date1))
+            if not os.path.isfile(fn_nc0) or not os.path.isfile(fn_nc1):
+                # Read mat structure
+                dsv = adcp.loaddata_vel(fn_mat)
+                # Check if start and end dates the same
+                date0 = str(pd.Timestamp(dsv.time[0].values).date())
+                date1 = str(pd.Timestamp(dsv.time[-1].values).date())
+                if date0 == date1:
+                    # Append entire dataset to list for concatenating
+                    dsv_daily.append(dsv)
+                else:
+                    # Split dsv to date0 and date1
+                    dsv0 = dsv.sel(time=date0).copy()
+                    # Append only correct date
+                    dsv_daily.append(dsv0)
+                    # Concatenate daily datasets and save to netcdf
+                    print('Concatenating daily datasets ...')
+                    dsd = xr.concat(dsv_daily)
+                    print('Saving daily dataset to netCDF ...')
+                    dsd.to_netcdf(fn_nc0)
+                    # Make new empty list and append the following day
+                    dsv_daily = []
+                    dsv1 = dsv.sel(time=date1).copy()
+                    dsv_daily.append(dsv1)
+                if i==(len(adsp.fns)-1):
+                    # Last file, save last netcdf
+                    print('Saving last daily dataset to netCDF ...')
+
 
 
